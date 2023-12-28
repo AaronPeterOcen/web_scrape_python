5837ff79c674afbaa8654087424739b8d0464913
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-12-28 20:30:14 +0300

    tbc

--- scrape.py
+++ scrape.py
@@ -3,21 +3,30 @@ from bs4 import BeautifulSoup
 
 resp = requests.get('https://news.ycombinator.com/newest')
 # print(resp.text)
+
 # using Beautiful Soup
 sp = BeautifulSoup(resp.text, 'html.parser')
 # print(sp)
 # print(sp.find_all('a'))
+
 # bs selectors
 links = sp.select('.titleline')
-votes = sp.select('.score')
+subtext = sp.select('.subtext')
 
 # print(votes[0].get('id'))
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
         title = links[idx].getText()
-        hn.append(title)
+        
+        # getting the links  using href
+        href = links[idx].get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            print(points)
+            hn.append({'title': title, 'link': href})
     return hn
 
-print(create_custom_hn(links, votes))
+(create_custom_hn(links, subtext))
